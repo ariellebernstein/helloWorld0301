94f188bf34cad0ff22e6282bc6cc8266ca8ff052
ariellebernstein <abernst5@terpmail.umd.edu>
2023-02-20 21:35:43 -0500

    editing for hello and about route

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World from Arielle Bernstein! I am adding my first code change.'
 
+@app.route('/hello')
+def hello():  # put application's code here
+    return render_template('hello.html')
+
 @app.route('/about')
 def hello():  # put application's code here
     return render_template('about.html')
